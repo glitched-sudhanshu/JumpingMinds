68ddb7271fde9adfd77a3462c63b160ba64f5b80
sudhanshu <r02.sudhanshu@gmail.com>
2023-04-10 19:27:55 +0530

    room added

--- app/src/main/java/com/example/tmm/views/fragments/DetailedItemFragment.kt
+++ app/src/main/java/com/example/tmm/views/fragments/DetailedItemFragment.kt
@@ -5,24 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.example.tmm.R
 import com.example.tmm.data.data_source.database.MarvelDatabase
 import com.example.tmm.databinding.FragmentDetailedItemBinding
 import com.example.tmm.domain.model.*
-import com.example.tmm.ui.viewmodels.MarvelListViewModel
 import com.example.tmm.ui.viewmodels.MarvelRoomViewModel
 import com.example.tmm.ui.viewmodels.MarvelRoomViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DetailedItemFragment : Fragment() {
 
     private var _binding: FragmentDetailedItemBinding? = null
-    private lateinit var viewModel : MarvelRoomViewModel
+    private lateinit var viewModel: MarvelRoomViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,6 +43,7 @@ class DetailedItemFragment : Fragment() {
 
         when (itemType) {
             is Character -> {
+                likedStatus(itemType)
                 val imageUrl = "${
                     itemType.thumbnail.replace("http",
                         "https")
@@ -54,13 +54,11 @@ class DetailedItemFragment : Fragment() {
                     .into(_binding!!.imgItem)
                 _binding!!.txtTitle.text = itemType.name
                 var descText = itemType.description
-                if(descText.isEmpty())descText = "N/A"
+                if (descText.isEmpty()) descText = "N/A"
                 _binding!!.txtDesc.text = "Description:\n${descText}"
                 _binding!!.txtNoOfComics.visibility = View.VISIBLE
                 _binding!!.txtNoOfComics.text = "No. of Comics : ${itemType.noOfComics}"
-
                 _binding!!.imgFav.visibility = View.VISIBLE
-                viewModel.insert(itemType)
             }
             is Creator -> {
                 val imageUrl = "${
@@ -90,7 +88,7 @@ class DetailedItemFragment : Fragment() {
                     .into(_binding!!.imgItem)
                 _binding!!.txtTitle.text = itemType.title
                 var descText = itemType.description
-                if(descText.isEmpty())descText = "N/A"
+                if (descText.isEmpty()) descText = "N/A"
                 _binding!!.txtDesc.text = "Description:\n${descText}"
                 _binding!!.txtPrice.text = "Price: $ ${itemType.price.toString()}"
                 _binding!!.txtPrice.visibility = View.VISIBLE
@@ -107,7 +105,7 @@ class DetailedItemFragment : Fragment() {
                     .into(_binding!!.imgItem)
                 _binding!!.txtTitle.text = itemType.title
                 var descText = itemType.description
-                if(descText.isEmpty())descText = "N/A"
+                if (descText.isEmpty()) descText = "N/A"
                 _binding!!.txtDesc.text = "Description:\n${descText}"
             }
             is MarvelSeries -> {
@@ -121,13 +119,52 @@ class DetailedItemFragment : Fragment() {
                     .into(_binding!!.imgItem)
                 _binding!!.txtTitle.text = itemType.title
                 var descText = itemType.description
-                if(descText.isEmpty())descText = "N/A"
+                if (descText.isEmpty()) descText = "N/A"
                 _binding!!.txtDesc.text = "Description:\n${descText}"
                 _binding!!.txtNoOfComics.text = "No. of Comics : ${itemType.noOfComics.toString()}"
-                _binding!!.txtNoOfCharacters.text = "No. of Characters : ${itemType.noOfCharacters.toString()}"
+                _binding!!.txtNoOfCharacters.text =
+                    "No. of Characters : ${itemType.noOfCharacters.toString()}"
                 _binding!!.txtNoOfComics.visibility = View.VISIBLE
                 _binding!!.txtNoOfCharacters.visibility = View.VISIBLE
 
+            }
+        }
+    }
+
+    //    private suspend fun likedStatus(character: Character) {
+//        val dbItem = viewModel.getCharacter(character.id)
+//        if(dbItem == null){
+//            _binding!!.imgFav.setImageResource(R.drawable.ic_favorite_border)
+//            _binding!!.imgFav.setOnClickListener {
+//                viewModel.insert(character)
+//
+//                _binding!!.imgFav.setImageResource(R.drawable.ic_favorite)
+//            }
+//        }else{
+//            _binding!!.imgFav.setImageResource(R.drawable.ic_favorite)
+//            _binding!!.imgFav.setOnClickListener {
+//                viewModel.delete(character)
+//                _binding!!.imgFav.setImageResource(R.drawable.ic_favorite_border)
+//            }
+//        }
+//    }
+    private fun likedStatus(character: Character) {
+        lifecycleScope.launch {
+            viewModel.allCharacters.observe(viewLifecycleOwner) { characters ->
+                val isLiked = characters.any { it.id == character.id }
+                if (isLiked) {
+                    _binding!!.imgFav.setImageResource(R.drawable.ic_favorite)
+                    _binding!!.imgFav.setOnClickListener {
+                        viewModel.delete(character)
+                        _binding!!.imgFav.setImageResource(R.drawable.ic_favorite_border)
+                    }
+                } else {
+                    _binding!!.imgFav.setImageResource(R.drawable.ic_favorite_border)
+                    _binding!!.imgFav.setOnClickListener {
+                        viewModel.insert(character)
+                        _binding!!.imgFav.setImageResource(R.drawable.ic_favorite)
+                    }
+                }
             }
         }
     }
